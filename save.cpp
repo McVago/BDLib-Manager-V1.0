596760956c4a2f7c3c4635e950d88f752cb0bca4
McVago <andresmcvago@gmail.com>
2018-03-10 20:07:28 -0600

    Mooving things

--- save.cpp
+++ save.cpp
@@ -2,19 +2,14 @@
 #include <fstream>
 #define UNIT_TESTING false
 void save(std::string file){
-
   std::string buffer;// be careful here
   long size;
   std::ofstream outfile (file, std::ofstream::binary);
-
-
   //this is for now, of course save will get data, not strings
   cin >> buffer;
-
   //read content of infile
   outfile.write(buffer, size);
   //always remember to close files
-  
   outfile.close();
 }
 
@@ -23,6 +18,9 @@ void save(std::string file){
 
 #if UNIT_TESTING
 int main(){
-
+  std::string file = "file.txt";
+  save(file);
+  std::cout << "Press enter to leave";
+  std::cin.get();
 }
 #endif
