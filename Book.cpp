195b0741fa0f4c8955a7444af7a55c6255591cf0
McVago <andresmcvago@gmail.com>
2018-03-12 10:33:14 -0600

    To finish for today

---
+++ Book.cpp
@@ -0,0 +1,13 @@
+class Book{
+  int ISBN;
+  std::string title,autor,year;
+  int amount;
+
+  Book(int ISBN, std::string title, std::string autor, std::string year, int amount);
+
+
+
+  Book::Book(int ISBN, std::string title, std::string autor, std::string year, int amount){
+
+  }
+}
