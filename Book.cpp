4c72b71df839f614595b957165767dc257c119e9
McVago <andresmcvago@gmail.com>
2018-03-12 17:07:12 -0600

    Trying my best Kappa

--- Book.cpp
+++ Book.cpp
@@ -2,12 +2,16 @@ class Book{
   int ISBN;
   std::string title,autor,year;
   int amount;
+  Book *next;
+  Book *previous;
 
-  Book(int ISBN, std::string title, std::string autor, std::string year, int amount);
+  Book(int isbn, std::string paramtitle, std::string paramautor, std::string paramyear, int paramamount):
+  ISBN{isbn}, title{paramtitle}, autor{paramautor}, year{paramyear}, amount{paramamount};{
+    *next = null;
+    *previous = null;
+  }
 
-
-
-  Book::Book(int ISBN, std::string title, std::string autor, std::string year, int amount){
+//  Book::Book(int ISBN, std::string title, std::string autor, std::string year, int amount){
 
   }
 }
